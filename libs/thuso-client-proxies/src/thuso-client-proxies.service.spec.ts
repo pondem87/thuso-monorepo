c754d6e9165817dbedde772d4697ef4d3b525290
tendai <pfidzetendai@gmail.com>
2025-04-30 20:22:08 +0200

    fixed broken test

--- libs/thuso-client-proxies/src/thuso-client-proxies.service.spec.ts
+++ libs/thuso-client-proxies/src/thuso-client-proxies.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ThusoClientProxiesService } from './thuso-client-proxies.service';
+import { LoggingService, mockedLoggingService } from '@lib/logging';
+import { LlmRmqClient, MgntRmqClient, WhatsappRmqClient } from '@lib/thuso-common';
 
 describe('ThusoClientProxiesService', () => {
   let service: ThusoClientProxiesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ThusoClientProxiesService],
+      providers: [
+        ThusoClientProxiesService,
+        {
+          provide: LoggingService,
+          useValue: mockedLoggingService
+        },
+        {
+          provide: MgntRmqClient,
+          useValue: {}
+        },
+        {
+          provide: LlmRmqClient,
+          useValue: {}
+        },
+        {
+          provide: WhatsappRmqClient,
+          useValue: {}
+        }
+      ],
     }).compile();
 
     service = module.get<ThusoClientProxiesService>(ThusoClientProxiesService);
